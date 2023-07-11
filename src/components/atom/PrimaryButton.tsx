ceec88b6919134c0d79ee6fda3dc59367dd74d31
planb-yano <takahiro.yano@plan-b.co.jp>
2023-07-12 01:10:27 +0900

    ボタンの汎用性を高める

--- src/components/atom/PrimaryButton.tsx
+++ src/components/atom/PrimaryButton.tsx
@@ -2,20 +2,16 @@ import React from "react";
 import { css } from "@emotion/react";
 
 type Props = {
-  className?: string;
   children: React.ReactNode;
+  variant?: string;
 };
 
-const PrimaryButton: React.FC<Props> = ({ className, children }) => {
-  return (
-    <button css={styles.base} className={className}>
-      {children}
-    </button>
-  );
+const PrimaryButton: React.FC<Props> = ({ children, variant }) => {
+  return <button css={styles.base(variant)}>{children}</button>;
 };
 
 const styles = {
-  base: css`
+  base: (variant?: string) => css`
     padding: 15px 100px;
     background-color: #0ea9fd;
     color: white;
@@ -24,10 +20,15 @@ const styles = {
     border-radius: 9999px;
     border: none;
     cursor: pointer;
-    transition: all .2s;
+    transition: all 0.2s;
     &:hover {
-      opacity: .9;
+      opacity: 0.9;
     }
+    ${variant === "md" &&
+    css`
+      padding: 7px 20px;
+      font-size: 15px;
+    `}
   `,
 };
 
