df655076db35f2d6f19d783ba358f62e19d82393
planb-yano <takahiro.yano@plan-b.co.jp>
2023-07-12 01:09:52 +0900

    色を選択できるようにした

--- src/components/atom/Icon2.tsx
+++ src/components/atom/Icon2.tsx
@@ -1,7 +1,12 @@
-import { SvgIconPropsSizeOverrides, SvgIconTypeMap } from "@mui/material";
+import {
+  SvgIconPropsColorOverrides,
+  SvgIconPropsSizeOverrides,
+  SvgIconTypeMap,
+} from "@mui/material";
 import { OverridableComponent } from "@mui/material/OverridableComponent";
 import { OverridableStringUnion } from "@mui/types";
 import React from "react";
+import { css } from "@emotion/react";
 
 type Props = {
   icon: OverridableComponent<SvgIconTypeMap<{}, "svg">> & {
@@ -14,11 +19,43 @@ type Props = {
         SvgIconPropsSizeOverrides
       >
     | undefined;
+  color?:
+    | OverridableStringUnion<
+        | "disabled"
+        | "inherit"
+        | "action"
+        | "primary"
+        | "secondary"
+        | "error"
+        | "info"
+        | "success"
+        | "warning",
+        SvgIconPropsColorOverrides
+      >
+    | undefined;
 };
 
-const Icon2: React.FC<Props> = ({ className, icon, fontSize = "large" }) => {
+const Icon2: React.FC<Props> = ({
+  className,
+  icon,
+  fontSize = "large",
+  color,
+}) => {
   const Icon = icon;
-  return <Icon className={className} fontSize={fontSize} />;
+  return (
+    <Icon
+      css={styles.base}
+      className={className}
+      fontSize={fontSize}
+      color={color}
+    />
+  );
+};
+
+const styles = {
+  base: css`
+    cursor: pointer;
+  `,
 };
 
 export default Icon2;
